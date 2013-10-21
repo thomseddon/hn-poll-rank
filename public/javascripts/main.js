d52f42d365c5662297ba0edf96037248a9aaea84
Thom Seddon <thom@nightworld.com>
2013-10-21 10:21:17 +0100

    Add Google Analytics

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,15 +1,9 @@
 
 
 (function(){
-  setInterval(function() {
-    window.location.reload();
-  }, 30000);
-
   var el = document.getElementById('post'),
     form = document.getElementById('form');
-    console.log(el, form)
   if (el && form) {
-    console.log('setup')
     form.onsubmit = function () { console.log('here')
       if (el.value.match(/id=/)) {
         el.value = el.value.split('id=')[1];
